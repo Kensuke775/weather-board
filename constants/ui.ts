7778aa7379be3d4d6d15a1eda090a1bf27c91ffe
Kensuke775 <t.ypebob96@gmail.com>
2026-07-04 16:57:55 +0900

    chore: UIレイアウト定数をconstants/ui.tsに切り出す (#6)

---
+++ constants/ui.ts
@@ -0,0 +1,22 @@
+export const BLUR_INTENSITY = 40;
+
+export const TOAST_DURATION = {
+  short: 1000,
+  default: 1500,
+} as const;
+
+export const AVATAR_BUTTON = {
+  size: 36,
+  borderRadius: 18,
+} as const;
+
+export const CARD_TEXT = {
+  noteHeight: 30,
+  tagRowHeight: 13,
+} as const;
+
+export const SLIDE_ANIMATION_DURATION = 300;
+
+export const ROOM_MEMBER_PANEL_WIDTH = 200;
+
+export const ROOM_HEADER_COLUMN_WIDTH = 80;
